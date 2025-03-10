0d85130b3990ba3d012bae250f8a150f4852b5b4
vikassahu123 <135429562+vikassahu123@users.noreply.github.com>
2025-03-10 16:36:43 +0530

    vikas sahu UC4 service layer with Autowired

---
+++ Spring_AddressBook_App_Development/src/main/java/com/springAddress_book/Spring_AddressBook_App_Development/DTO/AddressBookDTO.java
@@ -0,0 +1,40 @@
+package com.springAddress_book.Spring_AddressBook_App_Development.DTO;
+
+public class AddressBookDTO {
+
+    private String name;
+    private String email;
+    private String phone;
+
+    public AddressBookDTO() {}
+
+    public AddressBookDTO(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ Spring_AddressBook_App_Development/src/test/java/com/springAddress_book/Spring_AddressBook_App_Development/SpringAddressBookAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAddress_book.Spring_AddressBook_App_Development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddressBookAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
